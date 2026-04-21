cbe30264ba6acb43aebb336c5d64ed18df893609
Alex Morozov <lk4d4math@gmail.com>
2026-04-21 14:06:50 +0200

    fix: add missing _meta.lua for KOReader plugin metadata

---
+++ _meta.lua
@@ -0,0 +1,8 @@
+local _ = require("gettext")
+
+return {
+    name = "suwayomi_dl",
+    fullname = _("Suwayomi Downloader"),
+    description = _([[Browse and asynchronously download manga chapters from a Suwayomi server.]]),
+    version = "1.0.0",
+}
